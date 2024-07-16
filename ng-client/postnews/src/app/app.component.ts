cb6f1e1da2df0d670711dcc34a43420a31ec030f
dulefb <101865768+dulefb@users.noreply.github.com>
2024-07-16 23:18:06 +0200

    amall changes

--- ng-client/postnews/src/app/app.component.ts
+++ ng-client/postnews/src/app/app.component.ts
@@ -2,11 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ObjaveFeedComponent } from './components/objave-feed/objave-feed.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ObjaveFeedComponent,HttpClientModule],
+  imports: [
+    RouterOutlet,
+    ObjaveFeedComponent,
+    HttpClientModule,
+    HeaderComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
